41cebfb81c3d3b9d2c72d1e8fd6b68624a2198e7
Lucas Marcos <lucasmarcos@live.com>
2020-08-29 16:29:51 -0300

    fazendo coisas

--- client/src/main.js
+++ client/src/main.js
@@ -1,8 +1,133 @@
-const Abc = () => {
+const Usuarios = () => {
+  const [listaDeUsuarios, setListaDeUsuarios] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch("/api/usuarios")
+    .then(res => res.json())
+    .then(json => setListaDeUsuarios(json))
+    .catch(err => console.log(err));
+  });
+
   return (
-    <p></p>
+    <div>
+
+    <h1>usuários</h1>
+
+    <table>
+      <tbody>
+        {listaDeUsuarios.map(usuario =>
+          <tr key={usuario.id}>
+            <td>{usuario.id}</td>
+            <td>{usuario.nome}</td>
+          </tr>
+        )}
+      </tbody>
+    </table>
+
+    </div>
   );
 };
 
+const Tecnicos = () => {
+  const [listaDeTecnicos, setListaDeTecnicos] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch("/api/tecnicos")
+    .then(res => res.json())
+    .then(json => setListaDeTecnicos(json))
+    .catch(err => console.log(err));
+  });
+
+  return (
+    <div>
+
+    <h1>técnicos</h1>
+
+    <table>
+      <tbody>
+        {listaDeTecnicos.map(tecnico =>
+          <tr key={tecnico.id}>
+            <td>{tecnico.id}</td>
+            <td>{tecnico.nome}</td>
+          </tr>
+        )}
+      </tbody>
+    </table>
+
+    </div>
+  );
+};
+
+const Propriedades = () => {
+  const [listaDePropriedades, setlistaDePropriedades] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch("/api/propriedades")
+    .then(res => res.json())
+    .then(json => setlistaDePropriedades(json))
+    .catch(err => console.log(err));
+  });
+
+  return (
+    <div>
+
+    <h1>propriedades</h1>
+
+    <table>
+      <tbody>
+        {listaDePropriedades.map(Propriedade =>
+          <tr key={Propriedade.id}>
+            <td>{Propriedade.id}</td>
+            <td>{Propriedade.nome}</td>
+          </tr>
+        )}
+      </tbody>
+    </table>
+
+    </div>
+  );
+};
+
+const Animais = () => {
+  const [listaDeAnimais, setlistaDeAnimais] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch("/api/animais")
+    .then(res => res.json())
+    .then(json => setlistaDeAnimais(json))
+    .catch(err => console.log(err));
+  });
+
+  return (
+    <div>
+
+    <h1>animais</h1>
+
+    <table>
+      <tbody>
+        {listaDeAnimais.map(animal =>
+          <tr key={animal.id}>
+            <td>{animal.id}</td>
+            <td>{animal.nome}</td>
+          </tr>
+        )}
+      </tbody>
+    </table>
+
+    </div>
+  );
+};
+
+const Componente = () => {
+  return (
+    <div>
+      <Usuarios/>
+      <Tecnicos/>
+      <Propriedades/>
+      <Animais/>
+    </div>
+ );
+};
+
 const container = document.getElementById("container");
-ReactDOM.render(<Abc/>, container);
+ReactDOM.render(<Componente/>, container);
--- start.js
+++ start.js
@@ -10,9 +10,73 @@ fs.readFile("db/schema.sql", "utf-8", (error, schema) => {
   if(error) console.log(error);
 });
 
-app.get("/api", (req, res) => {
-  res.json({status: "ok"});
-});
+((app) => {
+  const lista_de_usuarios = [
+    { "id": "0", "nome": "usuario_0" },
+    { "id": "1", "nome": "usuario_1" },
+    { "id": "2", "nome": "usuario_2" }
+  ];
+
+  app.get("/api/usuarios", (_, res) => {
+    res.json(lista_de_usuarios);
+  });
+
+  app.get("/api/usuario/:id", (req, res) => {
+    const id = req.params.id;
+    res.json(lista_de_usuarios[id]);
+  });
+})(app);
+
+((app) => {
+  const lista_de_tecnicos = [
+    { "id": "0", "nome": "tecnico_0" },
+    { "id": "1", "nome": "tecnico_1" },
+    { "id": "2", "nome": "tecnico_2" }
+  ];
+
+  app.get("/api/tecnicos", (_, res) => {
+    res.json(lista_de_tecnicos);
+  });
+
+  app.get("/api/usuario/:id", (req, res) => {
+    const id = req.params.id;
+    res.json(lista_de_tecnicos[id]);
+  });
+})(app);
+
+((app) => {
+  const lista_de_propriedades = [
+    { "id": "0", "nome": "propriedade_0" },
+    { "id": "1", "nome": "propriedade_1" },
+    { "id": "2", "nome": "propriedade_2" }
+  ];
+
+  app.get("/api/propriedades", (_, res) => {
+    res.json(lista_de_propriedades);
+  });
+
+  app.get("/api/propriedade/:id", (req, res) => {
+    const id = req.params.id;
+    res.json(lista_de_propriedades[id]);
+  });
+})(app);
+
+((app) => {
+  const lista_de_animais = [
+    { "id": "0", "nome": "animal_0" },
+    { "id": "1", "nome": "animal_1" },
+    { "id": "2", "nome": "animal_2" }
+  ];
+
+  app.get("/api/animais", (_, res) => {
+    res.json(lista_de_animais);
+  });
+
+  app.get("/api/animal/:id", (req, res) => {
+    const id = req.params.id;
+    res.json(lista_de_animais[id]);
+  });
+})(app);
 
 app.use("/", express.static("client/www"));
 
